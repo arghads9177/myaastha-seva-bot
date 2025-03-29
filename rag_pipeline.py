574c7f01d422b01d1aefc6d8c0db194a83e64872
Argha Dey Sarkar <email2argha@gmail.com>
2025-03-29 16:32:54 +0530

    Hugging face embeding used

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -4,12 +4,14 @@ from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_groq import ChatGroq
 from langchain.memory import ConversationBufferMemory
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
 
 # Load environment variables
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["HUGGINGFACE_API_KEY"] = os.getenv("HUGGINGFACE_API_KEY")
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY").strip()
 
 def calculate_interest(principal, rate, time):
@@ -18,7 +20,8 @@ def calculate_interest(principal, rate, time):
 
 def load_rag_pipeline(vector_db_path="vector_store", model_name="GroqLlama"):
     """Loads the RAG pipeline with vector database and Groq’s LLaMA model."""
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    # embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
     vector_db = Chroma(persist_directory=vector_db_path, embedding_function=embeddings)
     llm = ChatGroq(model_name="llama-3.1-8b-instant")  # Replace with the correct Groq model name
     retriever = vector_db.as_retriever()
--- vector_store.py
+++ vector_store.py
@@ -3,16 +3,19 @@ import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 from langchain_community.document_loaders import TextLoader
 
 # Load environment variables
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["HUGGINGFACE_API_KEY"] = os.getenv("HUGGINGFACE_API_KEY")
 
 def store_embeddings(data_path="data", vector_db_path="vector_store"):
     """Loads text data from multiple files, splits, and stores embeddings in ChromaDB."""
-    embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    # embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+    embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
     all_docs = []
     for file in os.listdir(data_path):
         file_path = os.path.join(data_path, file)
